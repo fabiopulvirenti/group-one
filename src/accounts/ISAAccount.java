c537466ac586d0383e85ab3ee13285d7481452aa
younisraja16 <younisraja16@gmail.com>
2023-09-10 22:54:35 +0100

    Add comments and method to validate ISA accounts

--- src/accounts/ISAAccount.java
+++ src/accounts/ISAAccount.java
@@ -7,15 +7,58 @@ public class ISAAccount extends AbstractAccount{
     private int customerAge;
     private boolean ukResident;
     private boolean crownServant;
+
+    /**
+     * Inherits from the class AbstractAccount. Used to create ISA accounts for customers.
+     * Customer details are used to see if they meet the requirements and an account is
+     * created if requirements are met.
+     *
+     * @param customerID Customer's ID
+     * @param balance The balance in the account
+     * @param customerName Customer's name
+     * @param customerAge Customer's age
+     * @param isaType ISA Account type
+     * @param ukResident Whether customer is a UK resident
+     * @param crownServant Whether customer is a crown servant
+     */
     public ISAAccount (int customerID, float balance, String customerName, int customerAge, String isaType, boolean ukResident, boolean crownServant) {
         super(customerID, balance, customerName, AccountType.ISA);
-
         this.isaType = isaType;
         this.customerAge = customerAge;
         this.ukResident = ukResident;
         this.crownServant = crownServant;
+
+        if (validateAccount()) {
+            System.out.println("New " + isaType.toLowerCase() + " ISA account created.");
+        } else {
+            System.out.println("You do not meet the requirements to open a " + isaType.toLowerCase() + " ISA account. Please check the requirements and try again.");
+        }
     }
 
+    /**
+     * Check if the customer meets the requirements to open an ISA account.
+     *
+     * @return True or False dependent on if requirements are met.
+     */
+    private  boolean validateAccount(){
+        boolean valid = false;
+        if (isaType.equalsIgnoreCase("cash")){
+            return ((customerAge>=16) && (ukResident || crownServant));
+        } else if (isaType.equalsIgnoreCase("stocks and shares") || isaType.equalsIgnoreCase("innovative finance")) {
+            return ((customerAge >= 18) && (ukResident || crownServant));
+        } else if (isaType.equalsIgnoreCase("lifetime")) {
+            return ((customerAge >= 18 && customerAge < 40) && (ukResident || crownServant));
+        }
+        return valid;
+    }
+
+    /**
+     * Make an application to the relevant authorities on behalf of someone "who lacks the mental capacity"
+     * to open and manage an account themselves.
+     *
+     * @param countryName Checking where the customer resides. Process is country dependent.
+     * @return Lets user know an application has been made to the relevant authorities.
+     */
     public String mentallyChallengedApplication(String countryName){
         if (countryName.equals("Northern Ireland")) {
             return ("Application made to the Office of Care and Protection.");
