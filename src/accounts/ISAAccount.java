77cd5f7a43ded731202612f3d3b87a0cb26c1d73
younisraja16 <younisraja16@gmail.com>
2023-09-10 22:02:54 +0100

    Create ISAAccount class

---
+++ src/accounts/ISAAccount.java
@@ -0,0 +1,28 @@
+package accounts;
+
+public class ISAAccount extends AbstractAccount{
+
+    private final float apr = 2.75f;
+    private final String isaType;
+    private int customerAge;
+    private boolean ukResident;
+    private boolean crownServant;
+    public ISAAccount (int customerID, float balance, String customerName, int customerAge, String isaType, boolean ukResident, boolean crownServant) {
+        super(customerID, balance, customerName, AccountType.ISA);
+
+        this.isaType = isaType;
+        this.customerAge = customerAge;
+        this.ukResident = ukResident;
+        this.crownServant = crownServant;
+    }
+
+    public String mentallyChallengedApplication(String countryName){
+        if (countryName.equals("Northern Ireland")) {
+            return ("Application made to the Office of Care and Protection.");
+        } else if (countryName.equals("Scotland")) {
+            return ("Application made to the Office of the Public Guardian in Scotland.");
+        } else {
+            return ("Application made to the Court of Protection.");
+        }
+    }
+}
